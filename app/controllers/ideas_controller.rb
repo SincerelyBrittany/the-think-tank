91c00b48f559eedb4f035f497eac5fad0f4ebdf1
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-11 16:31:00 -0400

    creates new and create for ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,43 @@
 class IdeasController < ApplicationController
+    before_action :get_idea, only: [:show, :destroy, :edit, :update]
+
+    def index
+        @ideas = Idea.all
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        @idea.update(idea_params)
+        redirect_to @idea
+    end
+
+    def new
+        @idea = Idea.new
+    end 
+
+    def create
+        @idea = Idea.new(idea_params)
+        @idea.save
+        redirect_to @idea
+    end 
+
+    def destroy
+        @idea.destroy
+        redirect_to ideas_path
+    end
+
+    private
+
+    def get_idea
+        @idea = Idea.find(params[:id])
+    end
+
+    def idea_params
+        params.require(:idea).permit(:title, :description)
+    end
 end
