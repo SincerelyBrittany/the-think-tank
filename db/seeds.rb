47a8abc8d449b0d523544123af24b80f9c77ead5
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-11 16:03:20 -0400

    generates resources for ideas and generates controller for static pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = Idea.create(title: "idea 1", description: "this is the first idea")
+i = Idea.create(title: "idea 2", description: "this is the second idea")
